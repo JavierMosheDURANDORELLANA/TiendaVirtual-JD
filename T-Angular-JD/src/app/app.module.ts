c7f29575ecf5e60027d0e66fa06be024ada57411
Javier Moshe Durand Orellana <javierdurand.webmeister@gmail.com>
2020-05-29 02:25:51 -0500

    i librerias y componentes

--- T-Angular-JD/src/app/app.module.ts
+++ T-Angular-JD/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { TiendaComponent } from './tienda/tienda.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TiendaComponent,
+    NavbarComponent,
+    FooterComponent,
+    CatalogoComponent
   ],
   imports: [
     BrowserModule,
