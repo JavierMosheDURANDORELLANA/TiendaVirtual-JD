750709d92749e8ca0c1e6e3f46b8c2cf26467172
Javier Moshe Durand Orellana <javierdurand.webmeister@gmail.com>
2020-05-31 00:04:13 -0500

    Implementando el carrito de compras

--- T-Angular-JD/src/app/app.module.ts
+++ T-Angular-JD/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { TiendaComponent } from './tienda/tienda.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { CatalogoComponent } from './catalogo/catalogo.component';
+import { VermasComponent } from './vermas/vermas.component';
+import { DetalleComponent } from './vermas/detalle/detalle.component';
+import { KartComponent } from './kart/kart.component';
+import { ItemsComponent } from './kart/items/items.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +20,14 @@ import { CatalogoComponent } from './catalogo/catalogo.component';
     TiendaComponent,
     NavbarComponent,
     FooterComponent,
-    CatalogoComponent
+    CatalogoComponent,
+    VermasComponent,
+    DetalleComponent,
+    KartComponent,
+    ItemsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ T-Angular-JD/src/app/kart/kart.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kart',
+  templateUrl: './kart.component.html',
+  styleUrls: ['./kart.component.css'],
+})
+export class KartComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
