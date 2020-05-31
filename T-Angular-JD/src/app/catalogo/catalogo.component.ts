3346e83b4ce224bac02de77a2689360f2665f8f7
Javier Moshe Durand Orellana <javierdurand.webmeister@gmail.com>
2020-05-30 21:01:11 -0500

    culminando el catálogo

--- T-Angular-JD/src/app/catalogo/catalogo.component.ts
+++ T-Angular-JD/src/app/catalogo/catalogo.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { products } from '../products';
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
-  styleUrls: ['./catalogo.component.css']
+  styleUrls: ['./catalogo.component.css'],
 })
 export class CatalogoComponent implements OnInit {
+  products = products;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
