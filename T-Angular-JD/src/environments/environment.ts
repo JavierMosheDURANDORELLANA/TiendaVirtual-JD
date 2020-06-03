d43d904ff972a7926ff47038755e11dc7dd4fdfa
Javier Moshe Durand Orellana <javierdurand.webmeister@gmail.com>
2020-06-03 09:25:14 -0500

    instalando firebase

--- T-Angular-JD/src/environments/environment.ts
+++ T-Angular-JD/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDKiN1Bf7BThsbssJSTDTAbUQ5yiOE4Ewg',
+    authDomain: 'tiendavirtual-jd.firebaseapp.com',
+    databaseURL: 'https://tiendavirtual-jd.firebaseio.com',
+    projectId: 'tiendavirtual-jd',
+    storageBucket: 'tiendavirtual-jd.appspot.com',
+    messagingSenderId: '1061301289082',
+    appId: '1:1061301289082:web:c9a1b1c25756aa4c6c7bc3',
+  },
 };
 
 /*
